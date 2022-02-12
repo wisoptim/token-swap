8c52b079d85b23f9a63b54723863512e2ab37e68
DenB <bakuta.di@gmail.com>
2022-02-12 22:21:41 +0200

    lock function removed

--- contracts/TitanoSwap.sol
+++ contracts/TitanoSwap.sol
@@ -46,12 +46,12 @@ abstract contract Ownable is Context {
         _owner = newOwner;
     }
 
-    function lock(uint256 time) public virtual onlyOwner {
+/*     function lock(uint256 time) public virtual onlyOwner {
         _previousOwner = _owner;
         _owner = address(0);
         _lockTime = block.timestamp + time;
         emit OwnershipTransferred(_owner, address(0));
-    }
+    } */
 }
 
 abstract contract ReentrancyGuard {
